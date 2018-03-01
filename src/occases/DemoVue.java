acf91d3334d4465533869a59c60d40103394b5db
Trando <loic.coulon@condorcet.be>
2018-03-01 15:17:14 +0100

    premier commit : résolution du point 2 et 3

---
+++ src/occases/DemoVue.java
@@ -0,0 +1,19 @@
+package occases;
+
+
+public class DemoVue {
+    public static void main(String[] args) {
+        GarageVue gv=new GarageVue();
+        gv.affMsg("salut");
+        Voiture v =new Voiture("111","AAA","AAA",111,111,111);
+        gv.affMsg(v);
+        gv.affMsg("nom :");
+        String nom=gv.getMsg();
+        gv.affMsg("le nom encode est "+nom);
+        String tel=gv.getMsg("tel :");
+        gv.affMsg("le telephone encode est "+tel);
+        
+        Voiture v1= gv.encodeVoiture();
+        gv.affVoiture(v1);
+      }
+}
